893340b628c8e23080d76b770e6558dbe6b2bc30
ilona1009 <maliarenko.ilona@gmail.com>
2023-07-25 21:25:25 +0200

    fetch

--- src/img-api.js
+++ src/img-api.js
@@ -1,26 +1,33 @@
 import axios from "axios";
 
+export default class ApiPhotoService {
+    constructor() {
+        this.URL = 'https://pixabay.com/api/';
+        this.KEY = '38402918-c2c574f4e6229d1d2da3cdead';
+        this.PAGE = 1;
+        this.PER_PAGE = 40;
+        this.searchQuery = '';
+    }
+}
+
 async function fetchPhoto() {
-    const BASE_URl = 'https://pixabay.com/api/';
-    const API_KEY = '38402918-c2c574f4e6229d1d2da3cdead';
-    const CLIENT_SEARCH = 'cat';
     
     const searchParams = new URLSearchParams({
-        key: API_KEY,
-        q: CLIENT_SEARCH,
+        key: this.KEY,
+        q: this.searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        page: 1,
-        per_page: 40,
+        page: this.PAGE,
+        per_page: this.PER_PAGE,
     });
 
-    const url = `${BASE_URl}?${searchParams}`;
+    const url = `${this.URl}?${searchParams}`;
 
     try {
         const response = await axios.get(url); 
         if (!response.ok) {
-            console.log(response.data.hits);
+            console.log(response.data);
             throw new Error(response.statusText)
         }
         return response.data;
@@ -29,6 +36,6 @@ async function fetchPhoto() {
     };
 }
 
-fetchPhoto()
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
+import ApiPhotoService from './img-api';
 import fetchPhoto from './img-api';
 
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    bt_js: document.querySelector('.btn_js'),
+}
+
+refs.searchForm.addEventListener('submit', onSearch);
+// refs.bt_js.addEventListener('click', onClick)
+
+const apiPhotoService = new ApiPhotoService();
+
+async function onSearch(e) {
+    e.preventDefault();
+    this.searchQuery = e.currentTarget.elements.searchQuery.value;
+    console.log(this.searchQuery);
+        
+    try {
+        apiPhotoService.fetchPhoto().then(data => console.log(data))
+
+        // if (fetchResult) {
+        // }
+    } catch (error) {
+        console.log(error);
+    }
+}
